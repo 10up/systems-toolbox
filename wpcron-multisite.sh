09a3f953a60ad838478f7b5f01c53b5e0ade78f0
Jason DeWitt <jason.dewitt@10up.com>
2017-05-25 12:48:19 -0500

    adding lock file functionality directly to wp-cron script and not rely on user to configure

--- wpcron-multisite.sh
+++ wpcron-multisite.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/env bash
-
-# wpcron-multisite.sh
 #
 # bash script to execute pending cron events for all sites on a WordPress multisite install
 # set: `define('DISABLE_WP_CRON', true);` in wp-config.php to prevent cron from spawning
-# on page loads, and configure this script to run out of the system cron. It is recommended
-# to use a lock file to prevent multiple instances of this script running at the same time
-#
-# */5 * * * * flock -xn /tmp/wp-cli-cron.lck -c /opt/scripts/wpcron-multisite.sh >> /var/log/wpcron.log
-#
-# the script will output overall timing for wp-cron execution, as well as for each individual
+# on page loads, and configure this script to run out of the system cron.
+# The script will output overall timing for wp-cron execution, as well as for each individual
 # site on the multisite and for each wp-cron event, so capturing this output to a log file
 # is highly recommended
 # set $AS_ROOT variable to 1 if running this script as the root user
 
-WP_PATH="/path/to/wordpress/"
+WP_PATH="/var/www/html/wordpress/jasontest/"
+SITE_NAME="jasontest"
+LOCK_FILE="/tmp/wp-cron-${SITE_NAME}.lock"
 # path to wp-cli binary
-WPCLI="/usr/local/bin/wp"
+WPCLI="/usr/bin/wp"
 AS_ROOT=0
 FLAGS=""
+
+cleanup() {
+    # remove file lock if the script dies
+    rm -f "$LOCK_FILE"
+}
+
+if [ -e $LOCK_FILE ] ; then
+    exit 1
+fi
+
+# adding the trap specifically after checking for existnece of the lock file above
+# otherwise second script run will find the lock file, exit and then remove
+# the lock file, allowing another instance to run even if the first isn't done
+trap cleanup EXIT
+
+touch $LOCK_FILE
 
 if [ $AS_ROOT -eq 1 ]; then FLAGS="--allow-root"; fi
 
@@ -39,3 +51,5 @@ DIFF=$(echo $END - $START | bc)
 echo
 echo "total wp-cron run time $DIFF"
 echo
+sleep 60
+rm -f $LOCK_FILE
